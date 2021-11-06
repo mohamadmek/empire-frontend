3918ed75e4e7ece29122d6e649ad8d6fad3490ff
mohamadmek <45299765+mohamadmek@users.noreply.github.com>
2021-11-06 15:47:35 +0200

    Adding Main Colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple1f223a: "#1f223a",
+        purple110e1d: "#110e1d",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
